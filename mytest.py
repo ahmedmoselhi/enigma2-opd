38120b0974c2c4cd4465a6980953b444221f530b
formiano <formiano@formiano>
2020-03-26 12:46:32 +0100

    fix gigablue ultra ue

--- mytest.py
+++ mytest.py
@@ -778,10 +778,6 @@ import Components.InputDevice
 Components.InputDevice.InitInputDevices()
 import Components.InputHotplug
 
-profile("TimeZones")
-import Components.Timezones
-Components.Timezones.InitTimeZones()
-
 profile("SetupDevices")
 import Components.SetupDevices
 Components.SetupDevices.InitSetupDevices()
@@ -800,6 +796,10 @@ Components.RecordingConfig.InitRecordingConfig()
 profile("UsageConfig")
 import Components.UsageConfig
 Components.UsageConfig.InitUsageConfig()
+
+profile("TimeZones")
+import Components.Timezones
+Components.Timezones.InitTimeZones()
 
 profile("Init:DebugLogCheck")
 import Screens.LogManager
